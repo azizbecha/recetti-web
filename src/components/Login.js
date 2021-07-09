a8a384ca3d9fe23b42cf29d98ff86a466da30b19
AzizVirus <aziz07becha@gmail.com>
2021-07-09 23:36:29 +0100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { 
     Link, Route, Switch,
   } from "react-router-dom";
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../App.css";
 import "../Login.css"
@@ -13,7 +12,6 @@ import "firebase/auth";
 import app from "./auth/Firebase"
 import logo from '../assets/img/recetti-logo.png';
 var user = firebase.auth().currentUser;
-
 
 export default class Login extends React.Component {
     render() {
